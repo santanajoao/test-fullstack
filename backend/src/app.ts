3a579b5127da45a599179d26a0f3b88365b0f91f
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2024-03-14 22:26:28 -0300

    build: install zod

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,22 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+app.post('/clients', async (req, res) => {
+
+});
+
+app.get('/clients', async (req, res) => {
+
+});
+
+app.patch('/clients/:id', async (req, res) => {
+
+});
+
+app.get('/clients/:id', async (req, res) => {
+
+});
 
 export { app as default };
