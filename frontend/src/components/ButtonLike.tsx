60e5c3a95849a445aa2080517baac9479b466f88
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2024-03-17 09:44:41 -0300

    chore: create ButtonLike componente

---
+++ frontend/src/components/ButtonLike.tsx
@@ -0,0 +1,55 @@
+import { cn } from '@/lib/tailwind';
+import Link, { LinkProps } from 'next/link';
+import { ComponentProps } from 'react';
+
+type ButtonLikeProps = {
+  variant?: 'fill' | 'outline',
+  size?: 'default' | 'small',
+};
+
+const styles = ({ size, variant }: ButtonLikeProps) => cn(
+  'py-2 px-4 rounded-md text-center',
+  {
+    'text-base': size === 'default',
+    'text-sm': size === 'small',
+  },
+  {
+    'bg-[#e29836] text-white font-medium': variant === 'fill',
+    'border border-[#e29836] text-[#e29836] hover:text-white hover:bg-[#e29836] hover:font-medium transition-colors': variant === 'outline',
+  },
+);
+
+type ButtonProps = ComponentProps<'button'> & ButtonLikeProps;
+
+export function Button({ variant = 'fill', size = 'default', className, ...props }: ButtonProps) {
+  return (
+    <button
+      className={cn(
+        styles({ variant, size }),
+        className,
+      )}
+
+      {...props}
+    />
+  );
+}
+
+
+type AncorProps = ComponentProps<'a'> & LinkProps & ButtonLikeProps;
+
+export function Ancor({ variant = 'fill', size = 'default', className, ...props }: AncorProps) {
+  return (
+    <Link
+      className={cn(
+        styles({ variant, size }),
+        className,
+      )}
+      
+      {...props}
+    />
+  );
+}
+
+const ButtonLike = { Ancor, Button };
+
+export { ButtonLike as default };
