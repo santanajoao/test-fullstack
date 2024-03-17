4c4b8fbb036aa6d18700f9d9bad3ee0291d55585
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2024-03-17 09:58:53 -0300

    feat: create common layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
+import { FiUser } from 'react-icons/fi';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,16 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className={inter.className}>
         <Header />
-        {children}
+
+        <main className="pt-16 px-6 pb-12 max-w-6xl mx-auto">
+          <h1 className="text-2xl flex items-center font-medium text-[#333] border-b-2 border-b-[#f0f0f0] pb-6">
+            <FiUser className="mr-4 text-3xl text-black" />
+
+            Painel de clientes
+          </h1>
+
+          {children}
+        </main>
       </body>
     </html>
   );
